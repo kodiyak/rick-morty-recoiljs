22cf062d45f0027990214869a3b3fe48cef638b8
Math W <mathewsto51@gmail.com>
2021-12-08 18:38:59 -0300

    feat

--- src/components/modules/Characters/CharactersGridList/index.tsx
+++ src/components/modules/Characters/CharactersGridList/index.tsx
@@ -8,7 +8,7 @@ interface CharactersGridListProps {
 
 const CharactersGridList: React.FC<CharactersGridListProps> = ({ characters }) => {
   return (
-    <div className="w-full grid grid-cols-12 gap-4">
+    <div className="w-full grid md:grid-cols-8 2xl:grid-cols-12 gap-4">
       {characters.map((character, key) => (
         <CharacterCard character={character} key={`char${character.id}#${key}`} />
       ))}
